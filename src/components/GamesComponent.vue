ab66d1db867a97b55b66fb7fac6d19b97fab5c4c
renan1811 <rennlima477@gmail.com>
2025-10-17 12:02:05 -0300

    feat: arrumando os games #1

--- src/components/GamesComponent.vue
+++ src/components/GamesComponent.vue
@@ -5,40 +5,69 @@
     <nav>
         <ul>
             <li>
-                <h2>
-                    Decifre o filme
-                </h2>
-                <h3>
-                    1
-                </h3>
+                <h4>
+                    M
+                </h4>
+                <div>
+                    <h2>
+                        Decifre o filme
+                    </h2>
+                    <span>
+                        <h3>
+                            1
+                        </h3>
+                    </span>
+                </div>
+
             </li>
             <li>
-                <h2>
-                    Decifre o filme
-                </h2>
-                <h3>
-                    2
-                </h3>
+                <h4>
+                    M
+                </h4>
+                <div>
+                    <h2>
+                        Descubra o filme
+                    </h2>
+                    <h3>
+                        2
+                    </h3>
+                </div>
+
             </li>
             <li>
-                <h2>
-                    Decifre o filme
-                </h2>
-                <h3>
-                    3
-                </h3>
+                <h4>
+                    M
+                </h4>
+                <div>
+                    <h2>
+                        Descubra pela imagem
+                    </h2>
+                    <h3>
+                        3
+                    </h3>
+                </div>
+
             </li>
             <li>
-                <h2>
-                    Decifre o filme
-                </h2>
-                <h3>
-                    4
-                </h3>
+                <h4>
+                    M
+                </h4>
+                <div>
+                    <h2>
+                        Quem é mais famoso
+                    </h2>
+                    <h3>
+                        4
+                    </h3>
+                </div>
+
             </li>
             <li>
+                <h4>
+                    M
+                </h4>
                 <h2>
-                    Decifre o filme
+                    Acerte pelo elenco
                 </h2>
                 <h3>
                     5
@@ -59,18 +88,24 @@ nav ul li {
     width: 15vw;
     height: 35vh;
     color: white;
-    box-shadow: rgba(244, 255, 97, 0.35) 0px 5px 15px;
+    box-shadow: rgba(255, 255, 255, 0.35) 0px 5px 15px;
     margin: 1vw 1.5vw 1vw 1.5vw;
+    border-radius: 14px;
 }
 
+
 nav ul li h2 {
-    font-size: 2.5vw;
+    font-size: 1.5vw;
+    font-weight: bold;
+    margin-left: 0.5vw;
+    margin-top: 1vw;
+    width: 90%;
 }
 
 nav ul li h3 {
     font-size: 12vw;
     font-weight: bold;
-    color: white;
+    color: rgb(48, 45, 45);
     text-shadow:
         -2px -2px 0 #FFE100,
         2px -2px 0 #FFE100,
@@ -79,9 +114,13 @@ nav ul li h3 {
     font-family: 'Outfit', sans-serif;
     color: black;
     font-family: 'Inter', sans-serif;
-    position: relative; /* permite mover fora do li */
+    position: relative;
     right: 3vw;
     bottom: 1vw;
+    user-select: none;
+}
 
+nav ul li span h3 {
+    top: 1.5vw;
 }
 </style>
