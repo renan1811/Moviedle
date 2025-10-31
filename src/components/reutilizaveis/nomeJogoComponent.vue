4d4fe91228a32789304cd3894e5f8846e3df83ea
renan1811 <rennlima477@gmail.com>
2025-10-31 12:02:04 -0300

    feat: adicionando routers e views #2

---
+++ src/components/reutilizaveis/nomeJogoComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import '@mdi/font/css/materialdesignicons.min.css'
+</script>
+<template>
+    <div>
+        <h1>Nome do jogo</h1>
+        <span class="mdi mdi-information-outline"></span>
+    </div>
+    <div class="informacoes">
+        <h1>oiiiiiiiiiiii</h1>
+    </div>
+</template>
+<style scoped>
+
+div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+h1 {
+    font-size: 2rem;
+    color: white    ;
+    font-weight: bold;
+    margin: 0 1vw 0 0;
+}
+span {
+    font-size: 2.5rem;
+    color: #ffe100;
+}
+.informacoes {
+    opacity: 0;
+    background-color: white;
+    color: white;
+}
+span:hover .informacoes{
+    opacity: 1;
+}
+</style>
---
+++ src/views/decifrarView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NomeJogoComponent from '@/components/reutilizaveis/nomeJogoComponent.vue';
+
+</script>
+<template>
+    <NomeJogoComponent />
+</template>
+<style scoped>
+</style>
