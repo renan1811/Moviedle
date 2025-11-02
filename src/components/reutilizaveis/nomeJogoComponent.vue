15aed67f4a75e544d20ea01a48b1485e766615f0
renan1811 <rennlima477@gmail.com>
2025-11-02 15:41:23 -0300

    feat: melhorando pagina de decifrar filme #2

--- src/components/reutilizaveis/nomeJogoComponent.vue
+++ src/components/reutilizaveis/nomeJogoComponent.vue
@@ -1,15 +1,24 @@
 <script setup>
+import { ref } from 'vue';
 import '@mdi/font/css/materialdesignicons.min.css'
+const isHovered = ref(false);
+
+defineProps({
+    nome: String,
+    comoJogar: String,
+    desc: String,
+})
 </script>
 <template>
     <div class="jogo">
-  <h1>Nome do jogo</h1>
-  <span class="mdi mdi-information-outline">
-  <div class="informacoes">
-    <h1>oiiiiiiiiiiii</h1>
+  <h1>{{ nome }}</h1>
+  <span class="mdi mdi-information-outline" @mouseover="isHovered = true" @mouseleave="isHovered = false"></span>
+    </div>
+  <div class="informacoes" v-if="isHovered">
+    <h1>Como Jogar:</h1>
+    <p>{{ comoJogar }}</p>
   </div>
-  </span>
-</div>
+  <h1 class="desc">{{ desc }}</h1>
 </template>
 <style scoped>
 
@@ -17,7 +26,6 @@ div {
     display: flex;
     align-items: center;
     justify-content: center;
-
 }
 h1 {
     font-size: 2rem;
@@ -30,11 +38,26 @@ span {
     color: #ffe100;
 }
 .informacoes {
-    opacity: 1;
-    background-color: red;
-    color: red;
+    background-color: black;
+    display: block;
+    position: fixed;
+    left: 40%;
+    width: 20vw;
+    box-shadow: rgba(255, 225, 0, 0.4) 0px 3px 8px;
+    border-radius: 0.6vw;
 }
-span:hover .informacoes{
-    background-color: green;
+.informacoes h1 {
+    font-size: 1.3rem;
+    color: #ffe100;
+    margin: 0.3vw 0.8vw;
+}
+.informacoes p {
+    margin: 0.3vw 0.8vw;
+    color: white;
+}
+h1.desc {
+    display: flex;
+    justify-content: center;
+    font-size: 1.1rem;
 }
 </style>
--- src/views/decifrarView.vue
+++ src/views/decifrarView.vue
@@ -1,9 +1,23 @@
 <script setup>
 import NomeJogoComponent from '@/components/reutilizaveis/nomeJogoComponent.vue';
-
+import HeaderComponent from '@/components/reutilizaveis/HeaderComponent.vue';
+import { ref, onMounted } from 'vue';
+import api from '@/plugins/axios'
+const filmes = ref(null)
+const numAleatorio = Math.floor(Math.random() * 20)
+let filmeEscolhido = ref(null)
+console.log(numAleatorio)
+onMounted(async () => {
+    const response = await api.get('https://api.themoviedb.org/3/discover/movie?language=pt-BR&page=1')
+    filmes.value = response.data.results
+    filmeEscolhido.value = filmes[numAleatorio].title 
+})
 </script>
 <template>
-    <NomeJogoComponent />
+    <HeaderComponent />
+    <NomeJogoComponent nome="Decifrar o filme" comoJogar="Você vai receber um filme com o nome embaralhado, e tera que advinhar que filme é esse. A cada tentativa uma letra fica na posição correta." desc="Tente advinhar o filme com essa palavra: "/>
+    <h1> {{ filmeEscolhido}}</h1>
 </template>
+
 <style scoped>
 </style>
